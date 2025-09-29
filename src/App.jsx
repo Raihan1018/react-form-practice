fbd90a7c667ad5d219f0ab483ca2d7e5fdefce7f
Raihan <raihanuddinahmed10@gmail.com>
2025-09-29 22:19:07 +0600

    simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import SimpleForm from './components/SimpleForm'
+import FormAction from './components/FormAction'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='flex flex-col min-h-screen justify-center items-center'>
+      {/* <SimpleForm/> */}
+      <FormAction/>
+    </div>
   )
 }
 
