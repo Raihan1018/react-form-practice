35a13f22a08ca7747ca7d095f6cd2d715d2b5bb2
Raihan <raihanuddinahmed10@gmail.com>
2025-09-29 22:46:48 +0600

    uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import SimpleForm from './components/SimpleForm'
 import FormAction from './components/FormAction'
 import ControlledField from './components/ControlledField'
+import UnControlledForm from './components/UnControlledForm'
 
 const App = () => {
   return (
     <div className='p-16'>
       {/* <SimpleForm/> */}
       {/* <FormAction/> */}
-      <ControlledField/>
+      {/* <ControlledField/> */}
+      <UnControlledForm/>
     </div>
   )
 }
